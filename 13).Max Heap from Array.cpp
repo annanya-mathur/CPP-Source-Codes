a419ce7924e6027d48de2e4115054861d57f76f0
Annanya Mathur <68476475+annanya-mathur@users.noreply.github.com>
2022-02-21 09:43:29 -0800

    Initial Commit

---
+++ 13).Max Heap from Array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+void heapify (vector <int>&heap, int i, int n)
+{
+  int l = 2 * i + 1, r = 2 * i + 2, large = i;
+  if (l < n && heap[i] < heap[l])
+    large = l;
+  if (r < n && heap[i] < heap[r])
+    large = r;
+  if (large != i)
+    {
+      swap (heap[large], heap[i]);
+      heapify (heap, large, n);
+    }
+}
+
+int main ()
+{
+  int n;
+  cin >> n;
+  vector < int >heap (n, 0);
+  for (int i = 0; i < n; i++)
+    cin >> heap[i];
+  int index = (n - 1) / 2;
+  for (int i = index; i >= 0; i--)
+    heapify (heap, i, n);
+for (auto & i:heap)
+    cout << i << "\t";
+  return 0;
+}
