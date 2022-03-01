f669d732eea6081d4c9ff691cf412f546169a267
Annanya Mathur <68476475+annanya-mathur@users.noreply.github.com>
2022-02-28 22:04:31 -0800

    Commit Initial

---
+++ 22). Tabulation in fibonacci.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int fib (int n)
+{
+  int tabulation[n+1];
+  tabulation[0]=0;
+  tabulation[1]=1;
+  for(int i=2;i<=n;i++)
+  {
+      tabulation[i]=tabulation[i-1]+tabulation[i-2];
+  }
+  
+  return tabulation[n];
+}
+
+int main ()
+{
+  int n;
+  cin >> n;
+  
+  cout<<fib(n)<<endl;
+  return 0;
+}
