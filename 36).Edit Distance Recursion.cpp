ecb0c94313a2b55c0e7aba72004e6d2be0e617b6
Annanya Mathur <68476475+annanya-mathur@users.noreply.github.com>
2022-03-14 21:37:34 +0530

    Initial Commit

--- 36).Edit Distance Recursion.cpp
+++ 36).Edit Distance Recursion.cpp
@@ -12,8 +12,7 @@ int editDis (string s1, string s2, int n, int m)
   if (s1[n - 1] == s2[m - 1])
     return editDis (s1, s2, n - 1, m - 1);
   else
-    return 1 + min ((editDis (s1, s2, n, m - 1), editDis (s1, s2, n - 1, m)),
-		    editDis (s1, s2, n - 1, m - 1));
+    return 1 + min (editDis (s1, s2, n, m - 1),min( editDis (s1, s2, n - 1, m),editDis (s1, s2, n - 1, m - 1)));
 
 
 }
